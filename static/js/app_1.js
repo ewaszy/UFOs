dc29bd8aa3d9befab42bcfc5c54a38bf94f2cf8e
ewaszy <ewa.a.szyszkowska@gmail.com>
2021-09-05 11:29:33 -0500

    Challenge Complete

---
+++ static/js/app_1.js
@@ -0,0 +1,57 @@
+// Import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Simple JavaScript console.log statement
+function printHello() {
+    console.log("Hello there!");
+  }
+
+// Build table
+function buildTable(data) {
+    // Clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
+
+// Create a new function to handle what to do after an input is given
+function handleClick() {
+    // Grab datetime value from filter
+    let date = d3.select("#datetime").property("value");
+    // Set a default filter and save it to a new variable
+    let filteredData = tableData;
+
+    // Check if a date was entered and filter the data using that date
+    if (date) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+      };
+
+    // Rebuild the table using the filtered data
+    // If no date was entered, then filteredData will appear as 
+        // the original tableData
+    buildTable(filteredData);
+};
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
